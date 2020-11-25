d6499e97dd392d1328ca0179d64284052351f103
moshoodfakorede <moshood@armpension.com>
2020-11-25 15:01:41 +0100

    active router link

--- src/secure/components/Menu.vue
+++ src/secure/components/Menu.vue
@@ -2,10 +2,12 @@
     <nav id="sidebarMenu" class="col-md-3 col-lg-2 d-md-block bg-light sidebar collapse">
         <div class="sidebar-sticky pt-3">
             <ul class="nav flex-column">
-            <li class="nav-item">
-                <router-link to="/" class="nav-link active">Dashboard</router-link>
-                <router-link to="/users" class="nav-link">Users</router-link>
-            </li>
+                <li class="nav-item">
+                    <router-link to="/dashboard" active-class="active" class="nav-link">Dashboard</router-link>
+                </li>
+                <li class="nav-item">
+                    <router-link to="/users"  active-class="active" class="nav-link">Users</router-link>
+                </li>
             </ul>
         </div>
     </nav>

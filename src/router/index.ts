2e844354e8ee61aebb45738b2fe4b4a110fa863b
moshoodfakorede <moshood@armpension.com>
2020-11-28 01:05:36 +0100

    creating users

--- src/router/index.ts
+++ src/router/index.ts
@@ -4,6 +4,7 @@ import Login from '@/auth/Login.vue';
 import Index from '@/secure/Index.vue';
 import Dashboard from '@/secure/dashboard/Dashboard.vue';
 import Users from '@/secure/users/Users.vue';
+import CreateUser from '@/secure/users/CreateUser.vue';
 
 const routes: Array<RouteRecordRaw> = [
   { path: '/register', component: Register },
@@ -15,6 +16,7 @@ const routes: Array<RouteRecordRaw> = [
         { path: '', redirect: '/dashboard' },
         { path: '/dashboard', component: Dashboard },
         { path: '/users', component: Users },
+        { path: '/users/create', component: CreateUser },
       ]
   }
 ]
